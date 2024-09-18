b188845f0ae4b0bc5b7d5751f97dea558c5f94c5
jahidhasan6676 <jh18186676@gmail.com>
2024-09-18 09:15:48 +0600

    added the practice crackers file

---
+++ problem.js
@@ -0,0 +1,29 @@
+// function countZeros ( binary_num ) {
+//      count = 0;
+//     for(let num of binary_num){
+//         if(num === '0'){
+//             count += 1;
+//         }
+//     }
+//     return count;
+ 
+//  }
+//  console.log(countZeros('101010000'))
+
+// 
+// 
+
+// function calculate(arrOfNumbers){
+//     let sum = 0;
+//     let length = arrOfNumbers.length;
+//     for(let number of arrOfNumbers){
+//         let square = number * number;
+//         sum = sum + square;
+//          average = sum / length;
+
+//     }
+//     return average;
+// }
+
+// console.log(calculate( [4,6,10,12]
+// ))
